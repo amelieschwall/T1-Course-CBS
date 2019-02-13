b14122e8b83a19fdaafb0bbc0a82dedc1cb60e04
amelieschwall <47595891+amelieschwall@users.noreply.github.com>
2019-02-13 12:41:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hello, World")
+/* ".." Java Script will know it is a string (characters, numbers and special characters)
+variable = var 
+start with lower case letter and every Word after use a capital letter!
+variable identifier and assignment operator */
+
+/*create variable for first name*/
+var firstName = "John";
+/*create variable for last name*/
+var lastName ="Doe"
+
+/*cretae full name*/
+var fullName = firstName + " " + lastName /*to have a space between the first and last name*/ 
+console.log (fullName); 
+
+var age = 25;
+var addAge = 1;
+
+newAge = age + addAge;
+
+console.log(newAge);
+
+"This is the first line\nAnd this is the second" 
+
+/* from GitHub */
+// Create a number and assign to a variable
+let age = 26
+// Add
+let addResult = age + 1   // 27
+// Subtract
+let subtractResult = age - 26.5
+// Multiply
+let multiplyResult = age * 10
+// Divide
+// -0.5 // 260
+let divideResult = age / 2   // 13
+// Parenthesis can be used to make sure one operation happens before another letresult=(age+3)*7 //203
